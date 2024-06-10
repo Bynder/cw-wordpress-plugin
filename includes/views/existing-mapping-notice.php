f809c7822534ca4cc04342e1a01386d9dda408ee
alex.oakland <alex.oakland@bynder.com>
2024-06-10 13:37:36 +0100

    Escaped output in existing-mapping-notice.php

--- includes/views/existing-mapping-notice.php
+++ includes/views/existing-mapping-notice.php
@@ -1,3 +1,3 @@
 <div class="notice notice-info is-dismissible">
-	<p><?php printf( __( '<strong>NOTE:</strong> There can be only one %1$s per project template. You are editing an existing mapping (ID: %2$d).', 'content-workflow-by-bynder' ), $this->get( 'name' ), $this->get( 'id' ) ); ?></p>
+	<p><?php printf( esc_html__( '<strong>NOTE:</strong> There can be only one %1$s per project template. You are editing an existing mapping (ID: %2$d).', 'content-workflow-by-bynder' ), esc_html($this->get( 'name' )), esc_html($this->get( 'id' ) )); ?></p>
 </div>
