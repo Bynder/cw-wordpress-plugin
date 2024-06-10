86ed7e64c437ac7747a26dac4af2129ab71649a5
alex.oakland <alex.oakland@bynder.com>
2024-06-10 13:49:09 +0100

    Escaped output tmpl-gc-items-sync-progress.php

--- includes/views/tmpl-gc-items-sync-progress.php
+++ includes/views/tmpl-gc-items-sync-progress.php
@@ -1,11 +1,11 @@
-<legend class="screen-reader-text"><?php _e( 'Items Import Progress', 'content-workflow-by-bynder' ); ?></legend>
+<legend class="screen-reader-text"><?php esc_html_e( 'Items Import Progress', 'content-workflow-by-bynder' ); ?></legend>
 <table class="gc-sync-table">
 	<tbody>
 		<tr>
 			<th id="progress">
 				<div class="gc-progress-bar">
 					<div class="gc-progress-bar-partial" style="width: {{ data.percent }}%"><span>{{ data.percent }}%</span></div>
-					<button type="button" class="notice-dismiss gc-cancel-sync" title="<?php _e( 'Cancel Import', 'content-workflow-by-bynder' ); ?>"><span class="screen-reader-text"><?php _e( 'Cancel Import', 'content-workflow-by-bynder' ); ?></span></button>
+					<button type="button" class="notice-dismiss gc-cancel-sync" title="<?php esc_html_e( 'Cancel Import', 'content-workflow-by-bynder' ); ?>"><span class="screen-reader-text"><?php esc_html_e( 'Cancel Import', 'content-workflow-by-bynder' ); ?></span></button>
 				</div>
 			</th>
 		</tr>
