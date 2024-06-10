0d466bad733d3ef6c266db8718bb37eff14004a6
alex.oakland <alex.oakland@bynder.com>
2024-06-10 13:36:04 +0100

    Escaped output in tabs-wrapper.php

--- includes/views/tabs-wrapper.php
+++ includes/views/tabs-wrapper.php
@@ -2,16 +2,16 @@
 
 	<h5 class="nav-tab-wrapper gc-nav-tab-wrapper">
 	<?php foreach ( $this->get( 'tabs' ) as $tab_id => $tab ) : ?>
-		<a href="#<?php echo esc_attr( $tab['id'] ); ?>" class="nav-tab <?php echo isset( $tab['nav_class'] ) ? $tab['nav_class'] : ''; ?>"><?php echo $tab['label']; ?></a>
+		<a href="#<?php echo esc_attr( $tab['id'] ); ?>" class="nav-tab <?php echo esc_html( $tab['nav_class'] ?? '' ); ?>"><?php echo esc_html($tab['label']); ?></a>
 	<?php endforeach; ?>
 	</h5>
 
 	<?php $this->output( 'before_tabs_wrapper' ); ?>
 
 	<?php foreach ( $this->get( 'tabs' ) as $tab_id => $tab ) : ?>
-		<fieldset class="gc-template-tab <?php echo isset( $tab['tab_class'] ) ? $tab['tab_class'] : ''; ?>" id="<?php echo esc_attr( $tab['id'] ); ?>">
-		<legend class="screen-reader-text"><?php echo $tab['label']; ?></legend>
-		<?php echo $tab['content']; ?>
+		<fieldset class="gc-template-tab <?php echo esc_html($tab['tab_class'] ?? ''); ?>" id="<?php echo esc_attr( $tab['id'] ); ?>">
+		<legend class="screen-reader-text"><?php echo esc_html($tab['label']); ?></legend>
+		<?php echo esc_html($tab['content']); ?>
 		</fieldset>
 	<?php endforeach; ?>
 
