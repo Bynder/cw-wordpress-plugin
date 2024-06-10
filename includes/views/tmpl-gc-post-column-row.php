a5bb8e3124990a00aba32d5431fd2c25409474c1
alex.oakland <alex.oakland@bynder.com>
2024-06-10 13:47:59 +0100

    Escaped output tmpl-gc-post-column-row.php

--- includes/views/tmpl-gc-post-column-row.php
+++ includes/views/tmpl-gc-post-column-row.php
@@ -1,7 +1,7 @@
 <span class="gc-status-column" data-id="{{ data.id }}" data-item="{{ data.item }}" data-mapping="{{ data.mapping }}">
 <# if ( data.status.display_name ) { #>
 	<div class="gc-item-status">
-		<?php echo new self( 'underscore-data-status' ); ?>
+		<?php echo esc_html(new self( 'underscore-data-status' )); ?>
 	</div>
 <# } else { #>
 	&mdash;
