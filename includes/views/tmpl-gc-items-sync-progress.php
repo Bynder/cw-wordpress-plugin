e83a38a039ba0e5b662a1962639ee802d6a7d45e
alex.oakland <alex.oakland@bynder.com>
2024-06-11 14:21:55 +0100

    More updates around escaping output

--- includes/views/tmpl-gc-items-sync-progress.php
+++ includes/views/tmpl-gc-items-sync-progress.php
@@ -1,16 +1,22 @@
-<legend class="screen-reader-text"><?php _e( 'Items Import Progress', 'content-workflow-by-bynder' ); ?></legend>
+<legend class="screen-reader-text"><?php esc_html_e( 'Items Import Progress', 'content-workflow-by-bynder' ); ?></legend>
 <table class="gc-sync-table">
 	<tbody>
-		<tr>
-			<th id="progress">
-				<div class="gc-progress-bar">
-					<div class="gc-progress-bar-partial" style="width: {{ data.percent }}%"><span>{{ data.percent }}%</span></div>
-					<button type="button" class="notice-dismiss gc-cancel-sync" title="<?php _e( 'Cancel Import', 'content-workflow-by-bynder' ); ?>"><span class="screen-reader-text"><?php _e( 'Cancel Import', 'content-workflow-by-bynder' ); ?></span></button>
+	<tr>
+		<th id="progress">
+			<div class="gc-progress-bar">
+				<div class="gc-progress-bar-partial" style="width: {{ data.percent }}%"><span>{{ data.percent }}%</span>
 				</div>
-			</th>
-		</tr>
-		<# if ( data.loader ) { #>
-		<tr id="gc-reload-spinner"><td><span class="gc-loader spinner is-active"> Please Wait&hellip;</span></td></tr>
-		<# } #>
+				<button type="button" class="notice-dismiss gc-cancel-sync"
+						title="<?php esc_html_e( 'Cancel Import', 'content-workflow-by-bynder' ); ?>"><span
+						class="screen-reader-text"><?php esc_html_e( 'Cancel Import', 'content-workflow-by-bynder' ); ?></span>
+				</button>
+			</div>
+		</th>
+	</tr>
+	<# if ( data.loader ) { #>
+	<tr id="gc-reload-spinner">
+		<td><span class="gc-loader spinner is-active"> Please Wait&hellip;</span></td>
+	</tr>
+	<# } #>
 	</tbody>
 </table>
--- includes/views/tmpl-gc-post-column-row.php
+++ includes/views/tmpl-gc-post-column-row.php
@@ -1,11 +1,17 @@
 <span class="gc-status-column" data-id="{{ data.id }}" data-item="{{ data.item }}" data-mapping="{{ data.mapping }}">
 <# if ( data.status.display_name ) { #>
 	<div class="gc-item-status">
-		<?php echo new self( 'underscore-data-status' ); ?>
+		<?php
+		/**
+		 * Nothing to escape here as nothing is coming from the PHP side @see includes/views/underscore-data-status.php
+		 */
+		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo new self( 'underscore-data-status' );
+		?>
 	</div>
 <# } else { #>
 	&mdash;
 <# } #>
 </span>
 <?php
-	// echo "<# console.log( 'data', data ); #>";
+// echo "<# console.log( 'data', data ); #>";
