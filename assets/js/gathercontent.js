d6be65d5b1a462ce5f484761c28e5d26b53309a1
Gavin Allmond <gavin.allmond@bynder.com>
2024-06-25 14:52:22 +0100

    rebuilt assets from current main

--- assets/js/gathercontent.js
+++ assets/js/gathercontent.js
@@ -1,141 +1,117 @@
 /**
- * Content Workflow (by Bynder) - v1.0.0 - 2024-06-13
- *
+ * Content Workflow (by Bynder) - v1.0.0 - 2024-06-25
+ * 
  *
  * Copyright (c) 2024 Content Workflow (by Bynder)
  * Licensed under the GPLv2 license.
  */
 
-(function e(t, n, r) {
-	function s(o, u) {
-		if (!n[o]) {
-			if (!t[o]) {
-				var a = typeof require == "function" && require;
-				if (!u && a) return a(o, !0);
-				if (i) return i(o, !0);
-				var f = new Error("Cannot find module '" + o + "'");
-				throw f.code = "MODULE_NOT_FOUND", f
-			}
-			var l = n[o] = {exports: {}};
-			t[o][0].call(l.exports, function (e) {
-				var n = t[o][1][e];
-				return s(n ? n : e)
-			}, l, l.exports, e, t, n, r)
-		}
-		return n[o].exports
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+module.exports = function log() {
+	var method = 'log';
+
+	if (arguments[0] in window.console) {
+		method = Array.prototype.shift.apply(arguments);
 	}
 
-	var i = typeof require == "function" && require;
-	for (var o = 0; o < r.length; o++) s(r[o]);
-	return s
-})({
-	1: [function (require, module, exports) {
-		'use strict';
+	log.history = log.history || [];
+	log.history.push(arguments);
 
-		module.exports = function log() {
-			var method = 'log';
+	if (window.console && this.debug) {
+		window.console[method].apply(window.console, arguments);
+	}
+};
 
-			if (arguments[0] in window.console) {
-				method = Array.prototype.shift.apply(arguments);
-			}
+},{}],2:[function(require,module,exports){
+'use strict';
 
-			log.history = log.history || [];
-			log.history.push(arguments);
+window.GatherContent = window.GatherContent || {};
 
-			if (window.console && this.debug) {
-				window.console[method].apply(window.console, arguments);
-			}
-		};
+(function (window, document, $, gc, undefined) {
+	'use strict';
 
-	}, {}], 2: [function (require, module, exports) {
-		'use strict';
+	gc.el = function (id) {
+		return document.getElementById(id);
+	};
 
-		window.GatherContent = window.GatherContent || {};
+	gc.$id = function (id) {
+		return $(gc.el(id));
+	};
 
-		(function (window, document, $, gc, undefined) {
-			'use strict';
+	gc.log = require('./log.js').bind(gc);
 
-			gc.el = function (id) {
-				return document.getElementById(id);
-			};
+	var main = gc.main = {};
 
-			gc.$id = function (id) {
-				return $(gc.el(id));
-			};
+	main.init = function () {
+		$(document.body).on('click', '.gc-nav-tab-wrapper:not( .gc-nav-tab-wrapper-bb ) .nav-tab', main.changeTabs).on('click', '.gc-reveal-items', main.maybeReveal).on('click', '.gc-reveal-items-component', main.maybeRevealComponent);
 
-			gc.log = require('./log.js').bind(gc);
+		if (gc.queryargs.mapping) {
+			var $menu = gc.$id('toplevel_page_gathercontent-import');
+			$menu.find('.current').removeClass('current');
+			$menu.find('[href="edit.php?post_type=gc_templates"]').parent().addClass('current');
+		}
+	};
 
-			var main = gc.main = {};
+	main.changeTabs = function (evt) {
+		evt.preventDefault();
 
-			main.init = function () {
-				$(document.body).on('click', '.gc-nav-tab-wrapper:not( .gc-nav-tab-wrapper-bb ) .nav-tab', main.changeTabs).on('click', '.gc-reveal-items', main.maybeReveal).on('click', '.gc-reveal-items-component', main.maybeRevealComponent);
+		main.$tabNav = main.$tabNav || $('.gc-nav-tab-wrapper .nav-tab');
+		main.$tabs = main.$tabs || $('.gc-template-tab');
 
-				if (gc.queryargs.mapping) {
-					var $menu = gc.$id('toplevel_page_gathercontent-import');
-					$menu.find('.current').removeClass('current');
-					$menu.find('[href="edit.php?post_type=gc_templates"]').parent().addClass('current');
-				}
-			};
+		main.$tabNav.removeClass('nav-tab-active');
+		$(this).addClass('nav-tab-active');
+		main.$tabs.addClass('hidden');
+		gc.$id($(this).attr('href').substring(1)).removeClass('hidden');
+	};
 
-			main.changeTabs = function (evt) {
-				evt.preventDefault();
+	/**
+  * Accordion Toggle > Template Mapping: Field Description
+  * - Opens the drawer for a single field's description
+  */
+	main.maybeReveal = function (evt) {
+		var $this = $(this);
+		evt.preventDefault();
 
-				main.$tabNav = main.$tabNav || $('.gc-nav-tab-wrapper .nav-tab');
-				main.$tabs = main.$tabs || $('.gc-template-tab');
+		if ($this.hasClass('dashicons-arrow-right')) {
+			$this.removeClass('dashicons-arrow-right').addClass('dashicons-arrow-down');
+			$this.next().removeClass('hidden');
+		} else {
+			$this.removeClass('dashicons-arrow-down').addClass('dashicons-arrow-right');
+			$this.next().addClass('hidden');
+		}
+	};
 
-				main.$tabNav.removeClass('nav-tab-active');
-				$(this).addClass('nav-tab-active');
-				main.$tabs.addClass('hidden');
-				gc.$id($(this).attr('href').substring(1)).removeClass('hidden');
-			};
+	/**
+  * Accordion Toggle > Template Mapping: Component Fields
+  * - Opens the drawer for the component's description and subfields
+  */
+	main.maybeRevealComponent = function (evt) {
+		var $this = $(this);
+		evt.preventDefault();
 
-			/**
-			 * Accordion Toggle > Template Mapping: Field Description
-			 * - Opens the drawer for a single field's description
-			 */
-			main.maybeReveal = function (evt) {
-				var $this = $(this);
-				evt.preventDefault();
+		if ($this.hasClass('dashicons-arrow-right')) {
+			$this.closest('table').find('.gc-component-row').addClass('hidden');
+		} else {
+			$this.closest('table').find('.gc-component-row').removeClass('hidden');
+		}
+	};
 
-				if ($this.hasClass('dashicons-arrow-right')) {
-					$this.removeClass('dashicons-arrow-right').addClass('dashicons-arrow-down');
-					$this.next().removeClass('hidden');
-				} else {
-					$this.removeClass('dashicons-arrow-down').addClass('dashicons-arrow-right');
-					$this.next().addClass('hidden');
-				}
-			};
+	$(main.init);
 
-			/**
-			 * Accordion Toggle > Template Mapping: Component Fields
-			 * - Opens the drawer for the component's description and subfields
-			 */
-			main.maybeRevealComponent = function (evt) {
-				var $this = $(this);
-				evt.preventDefault();
+	window.onload = function () {
+		var textarea = jQuery('#system-info-textarea');
+		if (textarea.length) {
+			textarea.css('height', jQuery(window).height() * 0.7 + 'px');
+		}
+	};
 
-				if ($this.hasClass('dashicons-arrow-right')) {
-					$this.closest('table').find('.gc-component-row').addClass('hidden');
-				} else {
-					$this.closest('table').find('.gc-component-row').removeClass('hidden');
-				}
-			};
+	document.addEventListener('DOMContentLoaded', function () {
+		if (typeof redirectData !== 'undefined' && redirectData.redirectUrl) {
+			window.location = redirectData.redirectUrl;
+		}
+	});
+})(window, document, jQuery, window.GatherContent);
 
-			$(main.init);
-
-			window.onload = function () {
-				var textarea = jQuery('#system-info-textarea');
-				if (textarea.length) {
-					textarea.css('height', jQuery(window).height() * 0.7 + 'px');
-				}
-			};
-
-			document.addEventListener('DOMContentLoaded', function () {
-				if (typeof redirectData !== 'undefined' && redirectData.redirectUrl) {
-					window.location = redirectData.redirectUrl;
-				}
-			});
-		})(window, document, jQuery, window.GatherContent);
-
-	}, {"./log.js": 1}]
-}, {}, [2]);
+},{"./log.js":1}]},{},[2]);
