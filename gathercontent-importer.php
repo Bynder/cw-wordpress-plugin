22b30e50e31041c268883b7617e2de4fc459aaef
Chris Normansell <norman@gathercontent.com>
2024-07-12 10:18:51 +0100

    🔒️ Fixed being able to call the root plugin php file

--- gathercontent-importer.php
+++ gathercontent-importer.php
@@ -28,6 +28,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+ 
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly 
 
 // Useful global constants
 define( 'GATHERCONTENT_VERSION', '1.0.0' );
